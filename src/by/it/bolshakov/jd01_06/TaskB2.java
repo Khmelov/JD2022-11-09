331a7e15de8182c157d92dcba21b899044e33c83
Yaroslav Bolshakov <yvbv1999@mail.ru>
2022-11-30 03:18:10 +0300

    homework for lesson 6

---
+++ src/by/it/bolshakov/jd01_06/TaskB2.java
@@ -0,0 +1,19 @@
+package by.it.bolshakov.jd01_06;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class TaskB2 {
+    public static void main(String[] args) {
+        String text = Poem.text.replaceAll("[^а-яёА-ЯЁ.]", " ").replaceAll("\s+", " ");
+        String[] sentences = text.split("[.]");
+        String[] trimmedSentences = new String[sentences.length];
+        for (int i = 0; i < sentences.length; i++) {
+            trimmedSentences[i] = sentences[i].trim();
+        }
+        Arrays.sort(trimmedSentences, Comparator.comparingInt(String::length));
+        for (String sentence: trimmedSentences) {
+            System.out.print(sentence+"\n");
+        }
+    }
+}
