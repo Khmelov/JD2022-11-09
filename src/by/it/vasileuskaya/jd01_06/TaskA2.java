e6a14c25ab17e3523d9b798240a7c8d75fc97025
wasilewskaina <wasilewska.ina@gmail.com>
2022-11-30 09:58:38 +0100

    add Task6-A2

--- src/by/it/vasileuskaya/jd01_06/TaskA2.java
+++ src/by/it/vasileuskaya/jd01_06/TaskA2.java
@@ -1,4 +1,39 @@
 package by.it.vasileuskaya.jd01_06;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TaskA2 {
+    private static String[] words = new String[0];
+    private static int[] counter = new int[0];
+
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[а-яёА-ЯЁ]+");
+        Matcher matcher = pattern.matcher(Poem.text);
+        while (matcher.find()) {
+            String word = matcher.group();
+            checkWordForRepeat(word);
+        }
+        showNumberRepeat();
+    }
+
+    private static void checkWordForRepeat(String word) {
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word)) {
+                counter[i]++;
+                return;
+            }
+        }
+        words = Arrays.copyOf(words, words.length + 1);
+        words[words.length - 1] = word;
+        counter = Arrays.copyOf(counter, counter.length + 1);
+        counter[counter.length - 1] = 1;
+    }
+
+    private static void showNumberRepeat() {
+        for (int i = 0; i < words.length; i++) {
+            System.out.printf("%s=%d%n", words[i], counter[i]);
+        }
+    }
 }
