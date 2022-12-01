d09f595af80f2b8115b1368bcb17e5ae172b4ac4
Александр <asamatokhin@gmail.com>
2022-12-01 19:14:19 +0300

    Lesson 7 TaskA(1-3),TaskB1 complete

---
+++ src/by/it/samatokhin/jd01_07/Vector.java
@@ -0,0 +1,26 @@
+package by.it.samatokhin.jd01_07;
+
+class Vector extends Var {
+
+    private double[] value;
+
+    Vector(double[] value) {
+        this.value = value;
+    }
+
+    Vector(Vector vector) {
+        this.value = vector.value;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder("{");
+        String delimiter = "";
+        for (double element : value) {
+            sb.append(delimiter).append(element);
+            delimiter = ", ";
+        }
+        sb.append("}");
+        return sb.toString();
+    }
+}
