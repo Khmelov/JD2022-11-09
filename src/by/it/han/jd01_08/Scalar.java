88c600283bae7cdccd9727942468f07118f18efe
HaNiks <haniks2899@gmail.com>
2022-12-04 00:18:34 +0300

    taskB completed

--- src/by/it/han/jd01_08/Scalar.java
+++ src/by/it/han/jd01_08/Scalar.java
@@ -16,10 +16,16 @@ public class Scalar extends Var {
         this.value = Double.parseDouble(strScalar);
     }
 
+    public double getValue() {
+        return value;
+    }
+
     @Override
     public Var add(Var other) {
         if (other instanceof Scalar scalar) {
             return new Scalar(this.value + scalar.value);
+        } else if (other instanceof Vector vector) {
+            vector.add(this);
         }
         return super.add(other);
     }
@@ -28,6 +34,8 @@ public class Scalar extends Var {
     public Var sub(Var other) {
         if (other instanceof Scalar scalar) {
             return new Scalar(this.value - scalar.value);
+        } else if (other instanceof Vector vector) {
+            vector.sub(this);
         }
         return super.sub(other);
     }
@@ -36,14 +44,18 @@ public class Scalar extends Var {
     public Var mul(Var other) {
         if (other instanceof Scalar scalar) {
             return new Scalar(this.value * scalar.value);
+        } else if (other instanceof Vector vector) {
+            vector.mul(this);
         }
         return super.mul(other);
     }
 
     @Override
     public Var div(Var other) {
-        if (other instanceof Scalar scalar) {
+        if (other instanceof Scalar scalar && scalar.value != 0) {
             return new Scalar(this.value / scalar.value);
+        } else if (other instanceof Vector vector) {
+            vector.mul(this);
         }
         return super.div(other);
     }
