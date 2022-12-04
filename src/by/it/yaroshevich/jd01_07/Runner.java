3ddf501517963a400d2833dfae720681c9cbcd76
vlad_os <yaroshevich.vladislav2@gmail.com>
2022-12-04 16:37:50 +0300

    add lesson 07 tasks B2-3

--- src/by/it/yaroshevich/jd01_07/Runner.java
+++ src/by/it/yaroshevich/jd01_07/Runner.java
@@ -9,11 +9,14 @@ public class Runner {
         Var var2 = new Scalar(var1);
         Var var3 = new Scalar("3.333");
         double [] arrays = {1.0, 2.0, 4.0};
-        //Arrays.sort(arrays);
         Var var4 = new Vector(arrays);
+        Var var5 = new Vector(arrays);
+        Var var6 = new Vector("{1.0, 2.0, 4.0}");
         System.out.println(var1);
         System.out.println(var2);
         System.out.println(var3);
         System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
     }
 }
--- src/by/it/yaroshevich/jd01_07/Vector.java
+++ src/by/it/yaroshevich/jd01_07/Vector.java
@@ -7,11 +7,34 @@ public class Vector extends Var{
     private final double[] values;
 
     public Vector(double[] values) {
+
         this.values = values.clone();
+    }
+
+    public Vector(Vector vector) {
+
+        this.values=vector.values;
+    }
+
+    public Vector(String string){
+
+        this.values=string(string);
+    }
+
+    private double[] string(String strVector) {
+        String str = strVector.replaceAll("[{}]", "");
+        String[] vectors = str.split(",\\s?");
+        double[] arrayDouble = new double[vectors.length];
+        for (int i = 0; i < arrayDouble.length; i++) {
+            arrayDouble[i] = Double.parseDouble((vectors[i]));
+        }
+        return arrayDouble;
     }
 
     @Override
     public String toString() {
+
         return Arrays.toString(values).replace("[", "{").replace("]", "}");
     }
+
 }
