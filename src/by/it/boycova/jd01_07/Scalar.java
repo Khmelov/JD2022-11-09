dc633c314954b753923a2b33abbef25b2ecbd321
KseniyaBoycova <susonnya@gmail.com>
2022-12-05 20:06:00 +0100

    lesson 07 start

--- src/by/it/boycova/jd01_07/Scalar.java
+++ src/by/it/boycova/jd01_07/Scalar.java
@@ -20,9 +20,9 @@ public class Scalar extends Var {
 
 
 
-    ////@Override
-   ///// public String toString() {
-        ///return String.valueOf(value);
-    //}
+    @Override
+    public String toString() {
+        return String.valueOf(value);
+    }
 }
 
