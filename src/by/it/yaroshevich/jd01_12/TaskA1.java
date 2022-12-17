bd8cd04d6fcbb2c19432f05e1b8aaf6604d43885
vlad_os <yaroshevich.vladislav2@gmail.com>
2022-12-17 21:26:57 +0300

    add lesson 12 A1

---
+++ src/by/it/yaroshevich/jd01_12/TaskA1.java
@@ -0,0 +1,32 @@
+package by.it.yaroshevich.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class TaskA1 {
+
+    private final ArrayList<Integer> grades = new ArrayList<>();
+
+    public static void main(String[] args) {
+        TaskA1 solver = new TaskA1();
+        int size = ThreadLocalRandom.current().nextInt(20, 50);
+        for (int i = 0; i < size; i++) {
+            int grade = ThreadLocalRandom.current().nextInt(1, 11);
+            solver.grades.add(grade);
+        }
+        System.out.println(solver.grades);
+        solver.clearBad(solver.grades);
+        System.out.println(solver.grades);
+    }
+    private void clearBad(List<Integer> grades){
+        Iterator<Integer>iterator = grades.iterator();
+        while (iterator.hasNext()){
+            Integer currentGrade = iterator.next();
+            if (currentGrade < 4){
+                iterator.remove();
+            }
+        }
+    }
+}
