f8cc5c2b6f6e3ab53013fe9c8db78286f1774d05
HaNiks <haniks2899@gmail.com>
2022-12-21 13:58:42 +0300

    TaskB3

---
+++ src/by/it/han/jd01_12/TaskB3.java
@@ -0,0 +1,55 @@
+package by.it.han.jd01_12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class TaskB3 {
+    private static boolean isCrossed = false;
+
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Nik");
+        list.add("Nika");
+        list.add("Marina");
+        list.add("Seva");
+        list.add("Ivan");
+        LinkedList<String> list1 = new LinkedList<>();
+        list1.add("Nik");
+        list1.add("Nika");
+        list1.add("Marina");
+        list1.add("Seva");
+        list1.add("Ivan");
+        System.out.printf("%s=%s", "ArrayList", process(list));
+        System.out.printf("%s=%s", "ArrayList", process(list1));
+    }
+
+    private static String process(ArrayList<String> peoples) {
+        ArrayList<String> result = new ArrayList<>(peoples);
+        return getLast(result);
+    }
+
+    private static String process(LinkedList<String> peoples) {
+        LinkedList<String> result = new LinkedList<>(peoples);
+        return getLast(result);
+    }
+
+    private static String getLast(List<String> result) {
+        int remained = result.size();
+        while (remained > 1) {
+            Iterator<String> iterator = result.iterator();
+            while (iterator.hasNext() && remained > 1) {
+                iterator.next();
+                if (isCrossed) {
+                    isCrossed = false;
+                    iterator.remove();
+                    remained--;
+                    continue;
+                }
+                isCrossed = true;
+            }
+        }
+        return result.get(0);
+    }
+}
