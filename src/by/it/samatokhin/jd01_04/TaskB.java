db8390dadf5b9b8da4b611b8fc0d1f069d8e394c
Александр <asamatokhin@gmail.com>
2022-12-23 20:20:27 +0300

    Lesson 12 Tasks A, lesson4 TaskB 1-2, lesson 11 TaskB.

--- src/by/it/samatokhin/jd01_04/TaskB.java
+++ src/by/it/samatokhin/jd01_04/TaskB.java
@@ -1,5 +1,6 @@
 package by.it.samatokhin.jd01_04;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class TaskB {
@@ -38,21 +39,16 @@ public class TaskB {
             String secondName = surname[i];
             salary[line][0] = secondName;
             System.out.println("Введите зарплату для " + secondName);
-            Scanner sc = new Scanner(System.in);
-            System.out.println();
-            int quarter1 = sc.nextInt();
-            System.out.println();
-            int quarter2 = sc.nextInt();
-            System.out.println();
-            int quarter3 = sc.nextInt();
-            System.out.println();
-            int quarter4 = sc.nextInt();
+            int quarter1 = scanner.nextInt();
+            int quarter2 = scanner.nextInt();
+            int quarter3 = scanner.nextInt();
+            int quarter4 = scanner.nextInt();
             int sumQuarters = quarter1 + quarter2 + quarter3 + quarter4;
-            salary[line][1] = "\t" + Integer.toString(quarter1);
-            salary[line][2] = "\t" + Integer.toString(quarter2);
-            salary[line][3] = "\t" + Integer.toString(quarter3);
-            salary[line][4] = "\t" + Integer.toString(quarter4);
-            salary[line][5] = "\t" + Integer.toString(sumQuarters);
+            salary[line][1] = "\t\t" + Integer.toString(quarter1);
+            salary[line][2] = "\t\t" + Integer.toString(quarter2);
+            salary[line][3] = "\t\t" + Integer.toString(quarter3);
+            salary[line][4] = "\t\t" + Integer.toString(quarter4);
+            salary[line][5] = "\t\t" + Integer.toString(sumQuarters);
             allSum += sumQuarters;
         }
         salary[countGuys + 3][0] = "--------";
@@ -62,13 +58,13 @@ public class TaskB {
         salary[countGuys + 3][4] = "--------";
         salary[countGuys + 3][5] = "--------";
         salary[countGuys + 4][0] = "Итого";
-        salary[countGuys + 4][1] = Integer.toString(allSum);
+        salary[countGuys + 4][1] = "\t\t" + Integer.toString(allSum);
         salary[countGuys + 4][2] = "\t";
         salary[countGuys + 4][3] = "\t";
         salary[countGuys + 4][4] = "\t";
         salary[countGuys + 4][5] = "\t";
         salary[countGuys + 5][0] = "Средняя";
-        salary[countGuys + 5][1] = Double.toString(allSum/countGuys);
+        salary[countGuys + 5][1] = "\t\t" + (new DecimalFormat("0.0000")).format((Double.valueOf(allSum) / Double.valueOf(countGuys)) / 4);
         salary[countGuys + 5][2] = "\t";
         salary[countGuys + 5][3] = "\t";
         salary[countGuys + 5][4] = "\t";
