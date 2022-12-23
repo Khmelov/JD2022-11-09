fe995d2cf567931a567791a03c0afdb66517d7c7
HaNiks <haniks2899@gmail.com>
2022-12-23 12:49:22 +0300

    TaskA completed

---
+++ src/by/it/han/jd01_13/TaskA.java
@@ -0,0 +1,25 @@
+package by.it.han.jd01_13;
+
+import java.util.HashMap;
+
+public class TaskA {
+
+    public static void main(String[] args) {
+        try {
+            if (Math.random() > 0.5)
+                new HashMap<String, String>(null);
+            else
+                Integer.parseInt("привет");
+        } catch (NullPointerException | NumberFormatException e) {
+            String className = TaskA.class.getName();
+            StackTraceElement[] stackTrace = e.getStackTrace();
+            for (StackTraceElement stackTraceElement : stackTrace) {
+                if (stackTraceElement.getClassName().equals(className)) {
+                    System.out.println("name: " + e.getClass().getName());
+                    System.out.println("class: " + className);
+                    System.out.println("line: " + stackTraceElement.getLineNumber());
+                }
+            }
+        }
+    }
+}
