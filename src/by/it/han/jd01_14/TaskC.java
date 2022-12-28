cf286b58066637c1e8920dff58aea8a7d1aaa45b
HaNiks <haniks2899@gmail.com>
2022-12-28 11:59:49 +0300

    TaskC completed

--- src/by/it/han/jd01_14/TaskC.java
+++ src/by/it/han/jd01_14/TaskC.java
@@ -2,27 +2,33 @@ package by.it.han.jd01_14;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.PrintStream;
 
 public class TaskC {
 
     private static final String RESULT_FILE_NAME = "resultTaskC.txt";
+    private static final String _DIR = "dir:";
+    public static final String _FILE = "-file:";
+    private static boolean isAddToFile = false;
 
     public static void main(String[] args) {
         String path = getPath();
-        File dir = new File(path);
-        File[] files = dir.listFiles();
-        printFilesToConsole(files);
-        writeToResultFile(files);
+        File file = new File(path);
+        printFilesToConsole(file);
+        writeToResultFile(file);
     }
 
-    private static void writeToResultFile(File[] files) {
+    private static void writeToResultFile(File file) {
         String filePath = TaskA.getFilePath(RESULT_FILE_NAME);
-        try (PrintStream out = new PrintStream(filePath)) {
-            for (File file : files) {
-                out.println("dir:" + file.getName());
-                for (File listFile : file.listFiles()) {
-                    out.println("-file:" + listFile.getName());
+        try (PrintStream out = new PrintStream(new FileOutputStream(filePath, isAddToFile))) {
+            isAddToFile = true;
+            if (file.isFile()) {
+                out.println(_FILE + file.getName());
+            } else {
+                out.println(_DIR + file.getName());
+                for (File f : file.listFiles()) {
+                    writeToResultFile(f);
                 }
             }
         } catch (FileNotFoundException e) {
@@ -30,22 +36,23 @@ public class TaskC {
         }
     }
 
-    private static void printFilesToConsole(File[] files) {
-        for (File file : files) {
-            System.out.println("dir:" + file.getName());
-            for (File listFile : file.listFiles()) {
-                System.out.println("-file:" + listFile.getName());
+    private static void printFilesToConsole(File file) {
+        if (file.isFile()) {
+            System.out.println(_FILE + file.getName());
+        } else {
+            System.out.println(_DIR + file.getName());
+            for (File f : file.listFiles()) {
+                printFilesToConsole(f);
             }
         }
     }
 
     private static String getPath() {
         String property = System.getProperty("user.dir");
-        String path = property + File.separatorChar + "src"
+        return property + File.separatorChar + "src"
                 + File.separatorChar + "by"
                 + File.separatorChar + "it"
                 + File.separatorChar + "han"
                 + File.separatorChar;
-        return path;
     }
 }
