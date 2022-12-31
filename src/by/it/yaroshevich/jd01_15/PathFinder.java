56b40a2e4c91912397c651e867c59d3ee4207ebc
vlad_os <yaroshevich.vladislav2@gmail.com>
2022-12-31 22:38:23 +0300

    add lesson 15 A

---
+++ src/by/it/yaroshevich/jd01_15/PathFinder.java
@@ -0,0 +1,26 @@
+package by.it.yaroshevich.jd01_15;
+
+import by.it.yaroshevich.jd01_14.TaskA;
+
+import java.io.File;
+
+public class PathFinder {
+
+    private PathFinder(){
+
+    }
+
+    private static final String USER_DIR = "user.dir";
+    private static final String SRC = "src";
+
+    public static String getFilePath(String filename, Class<?> classFolder) {
+        String projectRoot = System.getProperty(USER_DIR);
+        String name = classFolder.getName();
+        String path = name
+                .replace(classFolder.getSimpleName(), "")
+                .replace(".", File.separator);
+        path = projectRoot+File.separator + SRC + File.separator + path;
+        return path  + filename;
+    }
+
+}
