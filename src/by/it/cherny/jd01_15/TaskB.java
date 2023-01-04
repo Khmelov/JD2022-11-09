63d2371a7c5a8f359ec93e886d58f7fa98228004
Sane-4ka <sasha12121299@gmail.com>
2023-01-04 23:16:02 +0300

    hw 15 taskA done

---
+++ src/by/it/cherny/jd01_15/TaskB.java
@@ -0,0 +1,15 @@
+package by.it.cherny.jd01_15;
+
+public class TaskB {
+    public static void main(String[] args) {
+        // comment1
+        // comment2
+        /*
+            comment3
+         */
+        /*
+            comment4
+         */
+        /**comment5*/
+    }
+}
