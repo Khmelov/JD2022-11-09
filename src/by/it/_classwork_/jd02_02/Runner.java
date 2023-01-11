35e9696c544ad428d336f116b34a4ab6fa1da69b
Alexander Khmelov <akhmelev@gmail.com>
2023-01-11 21:37:43 +0300

    classwork jd02_02 fix

--- src/by/it/_classwork_/jd02_02/Runner.java
+++ src/by/it/_classwork_/jd02_02/Runner.java
@@ -2,12 +2,9 @@ package by.it._classwork_.jd02_02;
 
 public class Runner {
     public static void main(String[] args) {
-        for (int i = 0; i < 10; i++) {
             Dispatcher dispatcher = new Dispatcher(100);
             CustomerQueue customerQueue = new CustomerQueue();
-            Store store = new Store("Sosedi"+i,dispatcher,customerQueue);
+            Store store = new Store("Sosedi",dispatcher,customerQueue);
             store.start();
-        }
-
     }
 }
