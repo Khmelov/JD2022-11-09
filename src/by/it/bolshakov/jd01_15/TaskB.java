e1099b6bdf0a6b61afa4e1c2ebac163b61fbbb16
Yaroslav Bolshakov <yvbv1999@mail.ru>
2023-01-15 17:16:35 +0300

    homework for lesson 15 A+B

--- src/by/it/bolshakov/jd01_15/TaskB.java
+++ src/by/it/bolshakov/jd01_15/TaskB.java
@@ -16,6 +16,9 @@ public class TaskB {
     public static final String OUTPUT_FILE = "TaskB.txt";
     public static final String INPUT_FILE = "TaskB.java";
     public static final Class<TaskB> CLASS_INSTANCE = TaskB.class;
+    public static final String MULTILINE_COMMENT_START = "/*";
+    public static final String MULTILINE_COMMENT_END = "*/";
+    public static final String SINGLE_COMMENT_START = "//";
 
     public static void main(String[] args) {
         /* Многострочный
@@ -44,12 +47,12 @@ public class TaskB {
     private static StringBuilder stripCommentsFromFile(String filepath) throws IOException {
         StringBuilder text = new StringBuilder(Files.readString(Path.of(filepath)));
         try {
-            while (true) text.replace(text.indexOf("/*"), text.indexOf("*/") + 2, "");
+            while (true) text.replace(text.indexOf(MULTILINE_COMMENT_START), text.indexOf(MULTILINE_COMMENT_END) + 2, "");
         } catch (StringIndexOutOfBoundsException e) {
 
         }
         try {
-            while (true) text.replace(text.indexOf("//"), text.indexOf("\n", text.indexOf("//")), "");
+            while (true) text.replace(text.indexOf(SINGLE_COMMENT_START), text.indexOf("\n", text.indexOf(SINGLE_COMMENT_START)), "");
         } catch (StringIndexOutOfBoundsException e) {
 
         }
