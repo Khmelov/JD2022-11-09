e741b056edc2b4677c97e8f38779df946a9aefe9
HaNiks <haniks2899@gmail.com>
2023-01-23 14:19:19 +0300

    HW2_5 A+B

---
+++ src/by/it/han/jd02_05/constants/Message.java
@@ -0,0 +1,8 @@
+package by.it.han.jd02_05.constants;
+
+public interface Message {
+    String WELCOME = "message.welcome";
+    String QUESTION = "message.question";
+    String PRINT_LOCAL = "message.printLocal";
+    String NO_SUCH_LOCALE = "message.noSuchLocale";
+}
