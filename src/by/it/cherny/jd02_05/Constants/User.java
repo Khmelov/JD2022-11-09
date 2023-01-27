f8658dd14ce38c4d9febe4838bb349a2e2e68f76
Sane-4ka <sasha12121299@gmail.com>
2023-01-27 23:38:54 +0300

    hw 2_05 a done

---
+++ src/by/it/cherny/jd02_05/Constants/User.java
@@ -0,0 +1,6 @@
+package by.it.cherny.jd02_05.Constants;
+
+public interface User {
+    String FIRST_NAME = "user.firstname";
+    String LAST_NAME = "user.lastname";
+}
---
+++ src/by/it/cherny/jd02_05/txt_files/convertor.bat
@@ -0,0 +1,4 @@
+set path=F:\backend\java8\bin
+native2ascii -encoding utf-8 text.txt ..\language\text.properties
+native2ascii -encoding utf-8 text_be.txt ..\language\text_be.properties
+native2ascii -encoding utf-8 text_ru.txt ..\language\text_ru.properties
