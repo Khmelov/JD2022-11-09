c6c30e7fd2cb920c390c7055e7be554953b8e738
Sane-4ka <sasha12121299@gmail.com>
2023-01-27 14:42:09 +0300

    hw 2_04 a done

--- src/by/it/cherny/calc/service/Parser.java
+++ src/by/it/cherny/calc/service/Parser.java
@@ -5,10 +5,23 @@ import by.it.cherny.calc.util.Patterns;
 import by.it.cherny.calc.repository.VarRepo;
 import by.it.cherny.calc.entity.Var;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Parser {
+
+    private final Map<String, Integer> mapOperation=Map.of(
+            "=", 0,
+            "+", 1,
+            "-", 1,
+            "*", 2,
+            "/", 2
+    );
+
     private final VarRepo varRepo;
     public Parser(VarRepo varRepo) {
         this.varRepo = varRepo;
@@ -16,26 +29,51 @@ public class Parser {
 
     public Var calc(String expression) throws CalcException {
 
-        String[] parts = expression.split(Patterns.OPERATION, 2);
-        if (parts.length==1){
-            return varRepo.create(parts[0]);
+        List<String> fixedValues = Arrays.asList(expression.split(Patterns.OPERATION));
+        List<String> values = new ArrayList<>(fixedValues);
+
+        List<String> operations = new ArrayList<>();
+        Matcher operationMatcher = Pattern.compile(Patterns.OPERATION).matcher(expression);
+        while (operationMatcher.find()) {
+            operations.add(operationMatcher.group());
         }
-        Var right = varRepo.create(parts[1]);
-        Pattern pattern = Pattern.compile(Patterns.OPERATION);
-        Matcher matcher = pattern.matcher(expression);
-        if (matcher.find()){
-            String operation = matcher.group();
-            if (operation.equals("=")){
-                return varRepo.save(parts[0],right);
-            }
-            Var left = varRepo.create(parts[0]);
-            switch(operation){
-                case"+":return left.add(right);
-                case"*":return left.mul(right);
-                case"-":return left.sub(right);
-                case"/":return left.div(right);
+        while (operations.size() > 0) {
+            int index = findOperationIndex(operations);
+            String operation = operations.remove(index);
+            String left = values.remove(index);
+            String right = values.remove(index);
+            Var tmpResult = calcOneOperation(left, operation, right);
+            values.add(index, tmpResult.toString());
+        }
+        return varRepo.create(values.get(0));
+    }
+
+    private int findOperationIndex(List<String> operations) {
+        int index=-1;
+        int pr=-1;
+        for (int i = 0; i < operations.size(); i++) {
+            Integer tmpPr = mapOperation.get(operations.get(i));
+            if (pr < tmpPr){
+                pr = tmpPr;
+                index=i;
             }
         }
-        return null;
+        return index;
+    }
+
+    private Var calcOneOperation(String leftStr, String operation, String rightStr) throws CalcException {
+        Var right = varRepo.create(rightStr);
+        if (operation.equals("=")){
+            return varRepo.save(leftStr,right);
+        }
+        Var left = varRepo.create(leftStr);
+
+        switch(operation){
+            case"+":return left.add(right);
+            case"*":return left.mul(right);
+            case"-":return left.sub(right);
+            case"/":return left.div(right);
+        }
+        throw new CalcException("incorrect data");
     }
 }
