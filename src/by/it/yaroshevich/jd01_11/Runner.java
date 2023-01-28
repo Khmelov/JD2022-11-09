f93d8f8cf229d73199323e4076fab536fc51a648
vlad_os <yaroshevich.vladislav2@gmail.com>
2023-01-28 23:47:19 +0300

    01_05 B + 01_11 B

--- src/by/it/yaroshevich/jd01_11/Runner.java
+++ src/by/it/yaroshevich/jd01_11/Runner.java
@@ -1,18 +1,30 @@
 package by.it.yaroshevich.jd01_11;
 
+import by.it.bolshakov.jd01_11.ListA;
+import by.it.bolshakov.jd01_11.ListB;
+
 import java.util.List;
 
 public class Runner {
     public static void main(String[] args) {
-        List<String> list = new ListA<>();
+        List<String> list = new ListB<>();
         list.add("one");
         list.add("two");
         list.add("three");
         list.add("four");
-        list.add("five");
+        list.add(1, "five");
+        List<String> list2 = new ListA<>();
+        list2.add("one");
+        list2.add("two");
+        list2.add("three");
+        list2.add("four");
+        list2.add("five");
         System.out.println(list);
         list.remove(3);
+        list.remove(0);
         System.out.println(list);
-        System.out.printf("element[1] = %s%n", list.get(1));
+        System.out.printf("element[1] = %s%n",list.get(1));
+        list.addAll(list2);
+        System.out.println(list);
     }
 }
