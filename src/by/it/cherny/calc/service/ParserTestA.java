27ea15c08e318855909b21f8780a444c15511e04
Sane-4ka <sasha12121299@gmail.com>
2023-01-29 16:58:46 +0300

    hw 01_05 b done, C half done

--- src/by/it/cherny/calc/service/ParserTestA.java
+++ src/by/it/cherny/calc/service/ParserTestA.java
@@ -25,7 +25,7 @@ public class ParserTestA {
     }
 
     @Test(timeout = 5000)
-    public void scalarB() throws CalcException {
+    public void scalarA1() throws CalcException {
         String expression = "B=A*3.5";
         double actual = Double.parseDouble(parser.calc(expression).toString());
         double expected = 25.55;
@@ -33,7 +33,7 @@ public class ParserTestA {
     }
 
     @Test(timeout = 5000)
-    public void scalarB1() throws CalcException {
+    public void scalarA2() throws CalcException {
         String expression = "B1=B+0.11*-5";
         double actual = Double.parseDouble(parser.calc(expression).toString());
         double expected = 25;
@@ -41,11 +41,34 @@ public class ParserTestA {
     }
 
     @Test(timeout = 5000)
-    public void scalarB2() throws CalcException {
+    public void scalarA3() throws CalcException {
         String expression = "B2=A/2-1";
         double actual = Double.parseDouble(parser.calc(expression).toString());
         double expected = 2.65;
         assertEquals(expected, actual, 1E-5);
     }
 
+    @Test(timeout = 5000)
+    public void scalarB() throws CalcException {
+        String expression = "C=B+(A*2)";
+        double actual = Double.parseDouble(parser.calc(expression).toString());
+        double expected = 40.15;
+        assertEquals(expected, actual, 1E-5);
+    }
+//
+//    @Test(timeout = 5000)
+//    public void scalarB1() throws CalcException {
+//        String expression = "D=((C-0.15)-20)/(7-5)";
+//        double actual = Double.parseDouble(parser.calc(expression).toString());
+//        double expected = 10;
+//        assertEquals(expected, actual, 1E-5);
+//    }
+//
+//    @Test(timeout = 5000)
+//    public void scalarB2() throws CalcException {
+//        String expression = "E={2,3}*(D/2)";
+//        String actual = String.valueOf(parser.calc(expression).toString());
+//        String expected = "{10,15}";
+//        assertEquals(expected, actual, 1E-5);
+//    }
 }
