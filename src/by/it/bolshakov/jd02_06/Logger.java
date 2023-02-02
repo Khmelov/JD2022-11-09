c9480e6eb69ae4f821d21b571410da513a7e236e
Yaroslav Bolshakov <yvbv1999@mail.ru>
2023-02-03 01:23:35 +0300

    homework for lesson jd_02_05 A+B and 02_06 A

---
+++ src/by/it/bolshakov/jd02_06/Logger.java
@@ -0,0 +1,50 @@
+package by.it.bolshakov.jd02_06;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+class Logger implements Log {
+
+    private static final String ERROR = "ERROR:";
+    private static final String INFO = "INFO:";
+    private volatile static Logger logger;
+    private final String filename = PathFinder.getFilePath("log.txt", Logger.class);
+
+    private Logger() {
+    }
+    public static Logger getLogger() {
+        Logger localLogger = logger;
+        if (localLogger == null) {
+            synchronized (Logger.class) {
+                localLogger = logger;
+                if (localLogger == null) {
+                    logger = localLogger = new Logger();
+                }
+            }
+        }
+        return localLogger;
+    }
+
+    @Override
+    public synchronized void info(String message) {
+        boolean append = true;
+        log(append, INFO, message);
+        System.out.printf("%s %s%n", INFO, message);
+    }
+
+    @Override
+    public synchronized void error(String message) {
+        boolean append = true;
+        log(append, ERROR, message);
+        System.err.printf("%s %s%n", ERROR, message);
+    }
+
+    private void log(boolean append, String TYPE, String message) {
+        try (PrintWriter printWriter = new PrintWriter(new FileWriter(filename, append))) {
+            printWriter.printf("%s %s%n", TYPE, message);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/by/it/bolshakov/jd02_06/Runner.java
@@ -0,0 +1,24 @@
+package by.it.bolshakov.jd02_06;
+
+public class Runner {
+
+    public static final int COUNT_THREAD = 10;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < COUNT_THREAD; i++) {
+            getThread(i).start();
+        }
+
+    }
+
+    private static Thread getThread(int i) {
+        return new Thread("#" + i) {
+            @Override
+            public void run() {
+                Logger logger = Logger.getLogger();
+                logger.error("ОШИБКА");
+                logger.info("СООБЩЕНИЕ");
+            }
+        };
+    }
+}
