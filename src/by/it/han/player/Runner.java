5c4c82020accc76780bc3eae962e64e6486fd8b8
HaNiks <haniks2899@gmail.com>
2023-02-04 11:59:44 +0300

    refactor Runner

--- src/by/it/han/player/Runner.java
+++ src/by/it/han/player/Runner.java
@@ -3,13 +3,22 @@ package by.it.han.player;
 import by.it.han.player.service.PlayerGeneratorTask;
 import by.it.han.player.service.PlayerReaderTask;
 
+import java.util.concurrent.ExecutionException;
+
 public class Runner {
-    public static void main(String[] args) {
+
+    public static final String NAME_OF_THREAD_STR = "Name of thread: %s ";
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         for (int i = 0; i < 10; i++) {
-            PlayerGeneratorTask playerGeneratorTask = new PlayerGeneratorTask(i);
-            playerGeneratorTask.start();
-            PlayerReaderTask playerReaderTask = new PlayerReaderTask(playerGeneratorTask);
-            playerReaderTask.start();
+            startThreads(i);
         }
+    }
+
+    private static void startThreads(int i) {
+        PlayerGeneratorTask playerGeneratorTask = new PlayerGeneratorTask(i);
+        System.out.printf(NAME_OF_THREAD_STR, playerGeneratorTask.call().getNameTask());
+        PlayerReaderTask playerReaderTask = new PlayerReaderTask(playerGeneratorTask);
+        playerReaderTask.start();
     }
 }
