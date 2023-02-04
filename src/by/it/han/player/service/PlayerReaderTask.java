928baa81b0cd3f9fec358f227decd870c5013333
HaNiks <haniks2899@gmail.com>
2023-02-04 11:24:56 +0300

    test task pre-pre-pre-result refactor

--- src/by/it/han/player/service/PlayerReaderTask.java
+++ src/by/it/han/player/service/PlayerReaderTask.java
@@ -1,6 +1,5 @@
 package by.it.han.player.service;
 
-import by.it.han.jd01_15.PatchFinder;
 import by.it.han.player.model.Player;
 
 import java.util.List;
@@ -9,7 +8,6 @@ public class PlayerReaderTask extends Thread {
     private static final int MIN_AGE = 24;
     private static final int MAX_AGE = 31;
     public static final int MAX_COUNT_PLAYERS = 5;
-    private static int countName = 0;
 
     private final PlayerGeneratorTask playerGeneratorTask;
 
@@ -25,7 +23,6 @@ public class PlayerReaderTask extends Thread {
     }
 
     public List<Player> getPlayersFromFile() {
-        String filePath = PatchFinder.getFilePath(++countName + "", PlayerReaderTask.class);
         return playerGeneratorTask.getPlayerList();
     }
 
@@ -36,9 +33,10 @@ public class PlayerReaderTask extends Thread {
     }
 
     public void printFirstPlayers(List<Player> playerList) {
-        playerList.stream()
+        List<String> names = playerList.stream()
                 .limit(MAX_COUNT_PLAYERS)
                 .map(p -> String.valueOf(p.getName()).toUpperCase())
-                .forEach(p -> System.out.print(p + " "));
+                .toList();
+        System.out.println(names);
     }
 }
